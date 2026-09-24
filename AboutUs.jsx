b2632e645110f89f1e031efb89fc6a03cfd8ad75
Anas Dingankar <31241941@vupune.ac.in>
2026-09-24 22:16:39 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery, your trusted destination for beautiful
+        and healthy houseplants.
+      </p>
+
+      <p>
+        At Paradise Nursery, we are passionate about bringing nature closer
+        to your home. We offer a wide variety of indoor plants carefully
+        selected to add beauty, freshness, and greenery to your living space.
+      </p>
+
+      <p>
+        Our mission is to make plant shopping simple and enjoyable by
+        providing quality plants at affordable prices. Whether you are an
+        experienced plant lover or just starting your collection, Paradise
+        Nursery has something for everyone.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
